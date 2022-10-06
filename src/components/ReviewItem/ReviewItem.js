5d6791a57197e7e7542b5df30b0d44d67cbdaa93
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-10-07 02:16:40 +0600

    Order component styling done

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,16 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 
 const ReviewItem = ({ product }) => {
     const { img, name, price, shipping } = product;
     return (
-        <div className='grid grid-cols-6 border-2 h-40 w-9/12 mx-auto my-8'>
-            <div className='col-span-1.5 w-full '>
-                <img className='object-fill h-40' src={img} alt="" />
+        <div className='grid grid-cols-6 border-2 h-40 w-7/12 mx-auto my-8'>
+            <div className='col-span-2 w-full '>
+                <img className='object-fill h-40 w-56' src={img} alt="" />
             </div>
             <div className='col-span-3 flex flex-col justify-center ml-4'>
                 <p className='text-2xl font-semibold'>{name}</p>
                 <p className='font-semibold text-xl my-2'>Price: ${price}</p>
                 <p className='text-lg'>Shipping Charge: ${shipping}</p>
+            </div>
+            <div className='col-span-1'>
+                <p className='mt-12 ml-8 '><FontAwesomeIcon className='w-12 h-12' icon={faTrashCan}></FontAwesomeIcon></p>
             </div>
         </div>
     );
