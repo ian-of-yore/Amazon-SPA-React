04715d06673b38f4f0779a4132d629e30a316bc1
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-10-06 21:39:14 +0600

    Module 53.1 done

---
+++ src/layout/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Shop from '../Shop/Shop';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Shop></Shop>
+        </div>
+    );
+};
+
+export default Main;
