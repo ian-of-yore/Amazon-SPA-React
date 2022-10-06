7899a9fd04d2b6297d0757ef426839149a05e862
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-10-07 02:36:53 +0600

    all styling for orders component done, only delete product button handler remains

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -5,9 +5,9 @@ import React from 'react';
 const ReviewItem = ({ product }) => {
     const { img, name, price, shipping } = product;
     return (
-        <div className='grid grid-cols-6 border-2 h-40 w-7/12 mx-auto my-8'>
+        <div className='grid grid-cols-6 border-2 h-40 w-7/12 mx-auto my-8 border-solid border-black rounded-md'>
             <div className='col-span-2 w-full '>
-                <img className='object-fill h-40 w-56' src={img} alt="" />
+                <img className='object-fill h-40 pb-1 pl-1 w-56' src={img} alt="" />
             </div>
             <div className='col-span-3 flex flex-col justify-center ml-4'>
                 <p className='text-2xl font-semibold'>{name}</p>
@@ -15,7 +15,9 @@ const ReviewItem = ({ product }) => {
                 <p className='text-lg'>Shipping Charge: ${shipping}</p>
             </div>
             <div className='col-span-1'>
-                <p className='mt-12 ml-8 '><FontAwesomeIcon className='w-12 h-12' icon={faTrashCan}></FontAwesomeIcon></p>
+                <button>
+                    <p className='mt-12 ml-8 text-red-600 bg-rose-100 p-3 rounded-full'><FontAwesomeIcon className='w-12 h-12' icon={faTrashCan}></FontAwesomeIcon></p>
+                </button>
             </div>
         </div>
     );
