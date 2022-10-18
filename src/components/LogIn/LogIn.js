dba1b3f9413d6b5f479de307d9c30d063407c640
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-10-18 12:37:43 +0600

    Basic LogIn/Register Form Style Done

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './LogIn.css'
 
 const LogIn = () => {
     return (
         <div className='form-container'>
-            <h1 className='text-3xl font-semibold'>Verify Your Credentials</h1>
-            <form>
-
+            <h2 className='form-title my-5'>Verify Your Credentials</h2>
+            <form >
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" required />
+                </div>
+                <p>Forgot Password? <button className='underline ml-1'>Reset</button></p>
+                <input className='btn-submit' type="submit" value="Login" />
             </form>
+            <p className='mt-4 text-center w-10/12'>New here? Create a<button className='underline ml-1'><Link to='/register'>New Account</Link></button></p>
+            
         </div>
     );
 };
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>This is the Register Component</h2>
+        <div className='form-container'>
+            <h2 className='form-title my-5'>Register Your Credentials</h2>
+            <form >
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="confirm">Confirm Password</label>
+                    <input type="password" name="confirm" required />
+                </div>
+                <input className='btn-submit' type="submit" value="Sign Up" />
+            </form>
+            <p className='mt-4 text-center w-10/12'>Already have an account?<button className='underline ml-1'><Link to='/login'>Log In</Link></button></p>
+            <p className='text-error'></p>
         </div>
     );
 };
