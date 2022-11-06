d32fea8c88eb54beb328ac628f5a7429d7b15c0a
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-11-06 19:43:43 +0600

    Pagination done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -39,15 +39,28 @@ const Shop = () => {
     useEffect(() => {
         const storedCart = getStoredCart();
         const savedCart = [];
-        for (const id in storedCart) {
-            const addedProduct = products.find(product => product._id === id);
-            if (addedProduct) {
-                const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
-                savedCart.push(addedProduct);
-            }
-        }
-        setCart(savedCart)
+        const ids = Object.keys(storedCart);
+        // console.log(ids)
+        fetch('http://localhost:5000/productsByIds', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(ids)
+        })
+            .then(res => res.json())
+            .then(data => {
+                for (const id in storedCart) {
+                    const addedProduct = data.find(product => product._id === id);
+                    if (addedProduct) {
+                        const quantity = storedCart[id];
+                        addedProduct.quantity = quantity;
+                        savedCart.push(addedProduct);
+                    }
+                }
+                setCart(savedCart)
+            })
+
     }, [products])
 
     const handleAddToCart = (seletedProduct) => {
